84be7c6d4961ec07a6315b283f2b94e6f96e2fc4
sstueluemen <sstueluemen@gmail.com>
2024-07-25 15:06:07 +0200

    Funktions

--- myBasicJavaWiederholen/src/Test/Functions_6.java
+++ myBasicJavaWiederholen/src/Test/Functions_6.java
@@ -3,8 +3,33 @@ package Test;
 public class Functions_6 {
 
 	public static void main(String[] args) {
+		// public -> Zugriffsmodifikator oder access modifier
+		// static -> Klass ebene 
+		// void -> ohne Rückgabe wert geben
 		// TODO Auto-generated method stub
-
+		// zu erst arbeits main Method
+		
+		//Rufen Sie zu erst welche du braust...
+		
+		int sum1 = 8;
+		int sum2 = 6;
+		int returnAntwort2 = summe(sum1, sum2);
+		System.out.println(returnAntwort2);
+		
+		//oder noch kurzer
+		int returnAntwort = summe(5,6); //Hier benutzt die Methode 
+		System.out.println(returnAntwort);
+		
+		
 	}
+	
+	//erste Funktion oder Metod
+	public static int summe(int ziffer1, int ziffer2) {
+		int antwort;
+		antwort = ziffer1 + ziffer2;
+		return antwort;	
+		}
+	
+	
 
 }
